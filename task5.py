b4a0c7f19dd7cb99c7f36d176c506544acf61e51
elvin1990huseynlii <161587697+elvin1990huseynlii@users.noreply.github.com>
2024-03-27 04:12:17 +0400

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,53 @@
+
+# Səhiyyə Sənayesi:
+# Tapşırıq: BMI Kalkulyatorunu tərtib edin.
+# Təsvir: Python funksiyalarından istifadə edərək Bədən Kütləsi İndeksi (BMI) kalkulyatoru yaradın. 
+# İstifadəçilərdən parametr olaraq çəkilərini (kiloqramla) və boylarını (metrlə) daxil etmələri təklif edilməlidir 
+# və proqram BMI-ni hesablayıb çap etməlidir. Əlavə olaraq, 
+# istifadəçiyə BMI kateqoriyası (məsələn, az çəki, normal çəki, artıq çəki və ya piylənmə) haqqında rəy bildirin.
+
+
+# def bodymassindex(height, weight):
+#     return round((weight / height**2),2)
+
+
+# h = float(input("Enter your height in meters: "))
+# w = float(input("Enter your weight in kg: "))
+
+
+# print("Welcome to the BMI calculator.")
+
+# bmi = bodymassindex(h, w)
+# print("Your BMI is: ", bmi)
+
+
+# if bmi <= 18.5:
+#     print("You are underweight.")
+# elif 18.5 < bmi <= 24.9:
+#     print("Your weight is normal.")
+# elif 25 < bmi <= 29.29:
+#     print("You are overweight.")
+# else:
+#     print("You are obese.")
+
+
+# Təhsil Sənayesi:
+# Tapşırıq: Tələbə Qiymət Kalkulyatoru.
+# Təsvir: Müxtəlif fənlər üzrə topladıqları ballara əsasən tələbələrin qiymətlərini hesablamaq üçün proqram yaradın. 
+# İstifadəçidən hər bir fənn üzrə xalları daxil etməyi, orta qiyməti hesablamağı və 
+# onların fəaliyyəti haqqında rəy bildirməyi təklif edin (məsələn, məktub qiymətləri). 
+# Orta hesablamaq və qiyməti müəyyən etmək üçün funksiyalardan istifadə edin və 
+# qiymətləndirmə meyarları üçün if-else ifadələrini birləşdirin. 
+# Qeyd: Bu taskdan bütün biliklərinizi maksimum istifadə edin və məntiqli kod yazmağa çalışın
+
+
+
+
+
+
+
+
+# Maliyyə Sənayesi:
+# Tapşırıq: Büdcə aləti yaradın
+# Təsvir: Tələbələr aylıq xərclər və gəlirlər üçün daxil olan Python proqramı yaradacaqlar. 
+# Funksiya ümumi xərcləri, ümumi gəliri almalı və ayın qalan büdcəsini hesablamalıdır.
